c16f509a9a86ce9b334317f948a706ec29ce155c
WraitheDX <joshua.t.bills86@gmail.com>
2020-09-28 11:18:21 -0700

    initial logger class
    need access to file_io to write logging out to file

---
+++ src/engine_systems/logger.cpp
@@ -0,0 +1,86 @@
+
+#include "logger.hpp"
+
+#include <ctime>
+
+LoggerLevel Logger::m_logger_level = LoggerLevel::LOG_LEVEL_PROGRESS;
+
+Logger::Logger( const LoggerLevel p_message_level ) :
+   m_message_level( p_message_level )
+{
+   
+}
+
+Logger::~Logger()
+{
+	m_message_stream << std::endl;
+   
+   // Only report the message if it's log level is within the logger's log level
+   if( m_message_level <= m_logger_level ) {
+      fprintf( stderr, "%s", m_message_stream.str().c_str() );
+      fflush( stderr );
+   }
+}
+
+std::ostringstream & Logger::log()
+{
+   // Obtain the current time
+   std::time_t l_time( std::time( 0 ) );
+   // Convert the current time into something more easily handled
+   std::tm l_current_time;
+   localtime_s( &l_current_time, &l_time );
+
+   // Append the month, adding 0 if needed to keep uniform alignment
+   if( ( l_current_time.tm_mon + 1 ) < 10 ) {
+      m_message_stream << 0;
+   }
+   m_message_stream << ( l_current_time.tm_mon + 1 ) << "/";
+
+   // Append day, adding 0 if needed to keep uniform alignment
+   if( l_current_time.tm_mday < 10 ) {
+      m_message_stream << 0;
+   }
+   m_message_stream << l_current_time.tm_mday << " ";
+
+   // Append hour, adding 0 if needed to keep uniform alignment
+   if( l_current_time.tm_hour < 10 ) {
+      m_message_stream << 0;
+   }
+   m_message_stream << l_current_time.tm_hour << ":";
+
+   // Append minutes, adding 0 if needed to keep uniform alignment
+   if( l_current_time.tm_min < 10 ) {
+      m_message_stream << 0;
+   }
+   m_message_stream << l_current_time.tm_min << ":";
+
+   // Append seconds, adding 0 if needed to keep uniform alignment
+   if( l_current_time.tm_sec < 10 )
+   {
+      m_message_stream << 0;
+   }
+   m_message_stream << l_current_time.tm_sec << " ";
+
+   // Append message log level
+   switch( m_message_level ) {
+      case LoggerLevel::LOG_LEVEL_ERROR:
+         m_message_stream << "**ERROR**: ";
+         break;
+      case LoggerLevel::LOG_LEVEL_WARNING:
+         m_message_stream << "!WARNING!: ";
+         break;
+      case LoggerLevel::LOG_LEVEL_PROGRESS:
+         m_message_stream << "~PROGRESS: ";
+         break;
+      case LoggerLevel::LOG_LEVEL_INFO:
+         m_message_stream << "--INFO---: ";
+         break;
+   }
+
+   return m_message_stream;
+}
+
+void Logger::set_logger_level( const LoggerLevel logger_level )
+{
+   m_logger_level = logger_level;
+}
--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,41 @@
 
 #include <iostream>
+#include <string>
+
+#ifdef _WIN32
+   #include <direct.h>
+#endif // _WIN32
+
+#include "engine_systems/logger.hpp"
+
+const
+std::string get_working_directory()
+{
+   std::string working_directory("");
+
+   // TODO (WraitheDX): Abstract below and the reference to direct.h above into a platform layer
+   #ifdef _WIN32
+   {
+      char buffer[FILENAME_MAX];
+      _getcwd( buffer, FILENAME_MAX );
+
+      working_directory = buffer;
+   }
+   #endif // _WIN32
+
+   return working_directory;
+}
 
 int main( char ** argument_strings, int argument_count )
 {
-	std::cout<< "Hello\n";
-	system("pause");
+   Logger::set_logger_level( LoggerLevel::LOG_LEVEL_INFO );
+   Logger( LoggerLevel::LOG_LEVEL_PROGRESS ).log() << "Realms Shattered is initializing";
+   Logger( LoggerLevel::LOG_LEVEL_PROGRESS ).log() << "Realms Shattered is initialized";
 
-	return 0;
+   std::cout<< "Hello\n";
+
+   Logger( LoggerLevel::LOG_LEVEL_PROGRESS ).log() << "Realms Shattered is shutting down";
+   system("pause");
+
+   return 0;
 }
