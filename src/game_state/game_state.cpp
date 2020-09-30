0d87f928e95a8802b264eabe015b19e4133cfbdf
WraitheDX <joshua.t.bills86@gmail.com>
2020-09-30 09:03:36 -0700

    Added ConfigFile, with saving and loading

--- src/game_state/game_state.cpp
+++ src/game_state/game_state.cpp
@@ -3,6 +3,7 @@
 
 #include <iostream>
 
+#include "../engine_systems/file_io.hpp"
 #include "../engine_systems/logger.hpp"
 #include "../entities/actor.hpp"
 
@@ -22,14 +23,46 @@ const bool GameState::initialize()
 {
    m_console.title_set( m_game_title );
 
-   m_language.commands_load( "data/language/en_commands.txt" );
-   m_language.text_load( "data/language/en_game_text.txt" );
+   ConfigFile config_file = FileIO::config_read();
+
+   if( config_file.m_language == "INVALID" ) {
+      // TODO (WraitheDX): Need function to ask the player their preferred language
+
+      FileIO::config_write( config_file );
+   }
+
+   // TODO (WraitheDX): Even though we should have a valid language set at this point, the language files
+   // might be missing, so check for them before moving on.
+
+   // Load the game and command text for the player's preferred language
+   m_language.commands_load( "data/language/" + config_file.m_language + "_commands.txt" );
+   m_language.text_load( "data/language/" + config_file.m_language + "_game_text.txt" );
+
+   // If any of the game dimensions are invalid, have the user set their preferred dimensions
+   if( config_file.m_game_height < m_console.m_print_height_min ||
+       config_file.m_game_width < m_console.m_print_width_min ||
+       config_file.m_game_offset_x < 0 ||
+       config_file.m_game_offset_y < 0 ) {
+      print_dimensions_setup();
+      
+      config_file.m_game_height = m_console.m_print_height;
+      config_file.m_game_width = m_console.m_print_width;
+      config_file.m_game_offset_x = m_console.m_print_offset_x;
+      config_file.m_game_offset_y = m_console.m_print_offset_y;
+
+      FileIO::config_write( config_file );
+   }
+
+   m_console.m_print_height = config_file.m_game_height;
+   m_console.m_print_width = config_file.m_game_width;
+   m_console.m_print_offset_x = config_file.m_game_offset_x;
+   m_console.m_print_offset_y = config_file.m_game_offset_y;
+
+   // This will eventually be in a new_game function or game_state
+   // player_name_get();
 
    // The game is now initialized and ready to begin the gameplay loop.
    Logger( LoggerLevel::LOG_LEVEL_PROGRESS ).log() << "Realms Shattered is initialized";
-   
-   print_dimensions_setup();
-   player_name_get();
    return true;
 }
 
