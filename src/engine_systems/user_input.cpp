501ff856c07018715df0faad335138f9760544aa
WraitheDX <joshua.t.bills86@gmail.com>
2020-10-01 00:53:57 -0700

    Aligned formatting and changed naming.

--- src/engine_systems/user_input.cpp
+++ src/engine_systems/user_input.cpp
@@ -3,10 +3,10 @@
 
 #include <iostream>
 #include <string>
-#include <filesystem>
-namespace fs = std::filesystem;
+#include <vector>
 
 #include "logger.hpp"
+#include "../entities/actor.hpp"
 
 const CommandTag UserInput::player_command_get( Language &language )
 {
@@ -19,6 +19,67 @@ const CommandTag UserInput::player_command_get( Language &language )
    return command_tag;
 }
 
+void UserInput::player_name_get( Console &console, GameData &game_data, Language &language )
+{
+   Logger( LoggerLevel::LOG_LEVEL_PROGRESS ).log() << "Asking player for name";
+
+   static const int TEXT_ENTER_NAME_POSITION_Y( 1 );
+
+   while( true ) {
+      console.print( language.text_tag_get( "tag_name_entry"), 0, TEXT_ENTER_NAME_POSITION_Y );
+
+      console.print( " > ", 0, TEXT_ENTER_NAME_POSITION_Y + 1 );
+      std::string name( player_string_get() );
+
+      console.clear();
+
+      if( name.empty() ) {
+         continue;
+      }
+
+      game_data.player_get()->name_set( name );
+      Logger( LoggerLevel::LOG_LEVEL_INFO ).log() << "Player entered name: " << game_data.player_get()->name_get();
+      break;
+   }
+}
+
+std::string UserInput::player_language_get( Console &console, Language &language){
+   Logger( LoggerLevel::LOG_LEVEL_PROGRESS ).log() << "Asking player for preferred language";
+
+   // all the available lanaguages need to be written in this array 
+   std::vector <std::string> language_vector;
+   language_vector.push_back( std::string( "en" ) );
+
+   const int TEXT_LANGUAGE_INSTRUCTION_POSITION_Y( console.height_get() - 4 );
+   const int TEXT_LANGUAGE_ENTRY_POSITION_Y( TEXT_LANGUAGE_INSTRUCTION_POSITION_Y + 1 );
+   const int TEXT_LANGUAGE_POSITION_Y( 1 );
+   bool check_lang = false;
+   while( !check_lang ) {
+      for( int i = 0 ; i < language_vector.size(); i++ ) {
+         console.print( language_vector[ i ], 2, TEXT_LANGUAGE_POSITION_Y + i );
+      }
+
+      console.print( "Write your preferred language (entry is case sensitive Ex: en)", 1, TEXT_LANGUAGE_INSTRUCTION_POSITION_Y );
+      console.print( "> ", 0, TEXT_LANGUAGE_ENTRY_POSITION_Y );
+     
+      std::string preferred_lang( "" );
+      std::getline( std::cin, preferred_lang );
+      for( int i( 0 ); i < language_vector.size(); i++ ){
+         if( preferred_lang == language_vector[ i ] ) {
+            console.clear();
+            return preferred_lang;
+         }
+      }
+      
+      console.print( "Invalid language, please try again." ), 1, TEXT_LANGUAGE_ENTRY_POSITION_Y + 1;
+      console.pause();
+      console.clear();
+   }
+
+   // If for any reason the while loop is escaped, we have an invalid language selection.
+   return "INVALID";
+}
+
 std::string UserInput::player_string_get()
 {
    std::string player_string( "" );
@@ -27,33 +88,3 @@ std::string UserInput::player_string_get()
 
    return player_string;
 }
-string UserInput::player_language_get( Console &console, Language &language){
-   // all the available lanaguages need to be written in this array 
-   std::string Languages = ["en"];
-   Logger( LoggerLevel::LOG_LEVEL_PROGRESS ).log() << "Asking player for preferred language \n";
-   static const int TEXT_ENTER_NAME_POSITION_Y( 1 );
-   for (int i = 0 ; i < Languages.size(); i++)
-      cout << i+1 <<" - "<< Languages[i] <<endl;
-   bool check_lang = false;
-   while( !check_lang ) {
-      cout << "Write your preferred language \t\tplease be careful to write it with case sensitive ex: en \n";
-      console.cursor_position_set( 0, TEXT_ENTER_NAME_POSITION_Y );
-     
-      std::string preferred_lang( "" );
-      std::getline( std::cin, preferred_lang );
-      for (int i = 0 ; i < Languages.size(); i++){
-         if (preferred_lang == Languages[i])
-            check_lang = true;
-         break;
-      }
-      
-
-      if( preferred_lang.empty() || check_lang == false) {
-         std:: cout << " Invalid language. Please try again\n";
-         continue;
-      }
-      return preferred_lang;      
-
-   }
-}
-
--- src/engine_systems/user_input.hpp
+++ src/engine_systems/user_input.hpp
@@ -9,18 +9,26 @@
 class UserInput final {
 public:
    /*
-   *  Retrieves player input and returns the relevant CommandTag
+   *  Retrieves player input and returns the relevant CommandTag.
    *
    *  TODO (WraitheDX): This needs to be expanded to allow for typing in multi-command strings.
    */
    const CommandTag player_command_get( Language &language );
 
    /*
-   *  Retrieves a raw string from the player's input
+   *  Asks the player to enter the language they prefer to play the game with, and returns the 2-character string of the language (Ex: en).
+   */
+   std::string player_language_get( Console &console, Language &language );
+
+   /*
+      Ask the player to enter the name of their character.
+   */
+   void player_name_get( Console &console, GameData &game_data, Language &language );
+
+   /*
+   *  Retrieves a raw string from the player's input.
    */
    std::string player_string_get();
-
-   string player_language_get(Console &console, Language &language );
 
 private:
 
