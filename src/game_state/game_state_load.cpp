d869606a586ed7e07ff0350f36b6892164154f96
Danyaaa <danyahamouda99@gmail.com>
2020-10-01 12:15:41 +0200

    small change in game_state_laod function

--- src/game_state/game_state_load.cpp
+++ src/game_state/game_state_load.cpp
@@ -5,24 +5,19 @@
 #include <sys/stat.h>
 #include <fstream>
 #include <string.h>
+#include <file_io.hpp>
 using namespace std;
 void GameState::game_state_load()
 {  char done = 'n';
    vector <string> saved_files;
+   bool found_save_folder;
    // read all the files in the save directory
-   DIR * dir = opendir ("/save");
-   struct dirent *entry;
-   if (dir == NULL)
-      cout<<" No save files found\n";
-   else   
-   {
-      string filename;
-      while((entry = readdir(dir)) != NULL){
-         filename = entry->d_name;
-         saved_files.push_back(filename.substr(0,filename.size()-4));
+   found_save_folder = FileIO::save_files_find(saved_files);
+   if (!found_save_folder)
+      {
+         cout<<" No save files found\n";
+         return;
       }
-   }
-   closedir(dir);
   repeat:
    cout<<"Your saved files are \t\t";
    for (auto i = saved_files.begin() ; i != saved_files.end(); i++){
@@ -34,23 +29,13 @@ void GameState::game_state_load()
    // to read the file 
 
    vector<string> file_content;
-   string path = "/save/" + choosen_file;
-   ifstream file(path);
-   if (!file.open()){
-      cout<<"Sorry, Could not find this file. \t\t press Y/y if you want to try again\n"
+   bool correct_name = FileIO::save_files_find(&file_content, &choosen_file);
+   if (!correct_name)
+     { cout<<"Sorry, Could not find this file. \t\t press Y/y if you want to try again\n"
       cin >> done;
       if (done == 'y' || done == 'Y')
          goto repeat;
-   }
-   else {
-      while(!file.eof()){
-         string file_line("");
-         getline(file, file_line);
-         file_content.push_back(file_line);
-      }
-      // now the file_content contains the file data 
-   }
-   
+     }
 
   // m_game_state_current = GameStateEnum::GAME_STATE_PLAYER_HUB;
 }
